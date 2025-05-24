750e26071be542fd3477de1093edf5d90ab28cba
Riajul Islam <Riajul5@hotmail.co.uk>
2025-05-24 12:37:27 +0100

    Added code comments for app.py and docker-compose.yml file

--- Docker-challenge/app/app.py
+++ Docker-challenge/app/app.py
@@ -1,16 +1,21 @@
+# Importing packages
 import os
 from flask import Flask
 import redis
 
 app = Flask(__name__)
+
+# Setting environment variables
 redis_host = os.getenv('REDIS_HOST', 'redisdb')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
 r = redis.Redis(host=redis_host, port=redis_port)
 
+# Creating route route for simple application
 @app.route('/')
 def hello_world():
     return 'CoderCo Containers Session!'
 
+# Creating count route using redis visit count
 @app.route('/count')
 def visit_count():
     count = r.incr('visits')
